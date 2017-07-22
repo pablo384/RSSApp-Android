3f53b7f43207cf2135733bcd8cadfce792fc7b87
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-22 00:00:04 -0400

    Arreglado envio de URL via intent

--- app/src/main/java/com/example/faustino/rssprueba/MainActivity.java
+++ app/src/main/java/com/example/faustino/rssprueba/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button btnDL, btnLD, BtnNac;
 
-   //public static final String FEED_URL = "feed_url";
+   public static final String FEED_URL = "feed_url";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,37 +17,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnDL = (Button) findViewById(R.id.buttonRSSDL);
-//        btnLD = (Button) findViewById(R.id.buttonRSSLD);
-//        BtnNac = (Button) findViewById(R.id.rssEN);
+        btnLD = (Button) findViewById(R.id.buttonRSSLD);
+        BtnNac = (Button) findViewById(R.id.rssEN);
 
         btnDL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RssDetails.class);
-                //intent.putExtra(FEED_URL, R.string.Rss1);
+                intent.putExtra(FEED_URL, getResources().getString(R.string.URLRss1));
                 startActivity(intent);
             }
         });
 
 
-//        btnLD.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, RssDetails.class);
-//                //intent.putExtra(FEED_URL, R.string.Rss2);
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//        BtnNac.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, RssDetails.class);
-//                //intent.putExtra(FEED_URL, R.string.Rss3);
-//                startActivity(intent);
-//            }
-//        });
+        btnLD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RssDetails.class);
+                intent.putExtra(FEED_URL, getResources().getString(R.string.URLRss2));
+                startActivity(intent);
+            }
+        });
+
+
+        BtnNac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RssDetails.class);
+                //intent.putExtra(FEED_URL, R.string.Rss3);
+                intent.putExtra(FEED_URL, getResources().getString(R.string.URLRss3));
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/faustino/rssprueba/RssDetails.java
+++ app/src/main/java/com/example/faustino/rssprueba/RssDetails.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 //import java.util.List;
 
-//import static com.example.faustino.rssprueba.MainActivity.FEED_URL;
+import static com.example.faustino.rssprueba.MainActivity.FEED_URL;
 
 public class RssDetails extends AppCompatActivity {
     //ListView feedlistview = (ListView) findViewById(R.id.my_list);
@@ -30,26 +31,17 @@ public class RssDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rssdetails);
 
-        final String[] datos =
-                new String[]{"Pablo1","Elem2","Elem3","Elem4","Elem5"};
-
-        ArrayAdapter<String> adaptador =
-                new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, datos);
         lstOpciones = (ListView)findViewById(R.id.LstOpciones);
 
 
 
 
-        String url = "https://www.diariolibre.com/rss/portada.xml"; //(String) getIntent().getExtras().get(FEED_URL);
+        String url = getIntent().getStringExtra(FEED_URL);
         Parser parser = new Parser();
         parser.onFinish(new Parser.OnTaskCompleted() {
             @Override
             public void onTaskCompleted(ArrayList<Article> list) {
                 lstOpciones.setAdapter(new FeedAdapter(RssDetails.this,R.layout.listitem_titular,list));
-
-
-
             }
 
             @Override
@@ -57,7 +49,13 @@ public class RssDetails extends AppCompatActivity {
 
             }
         });
-        parser.execute(url);
+        try{
+            parser.execute(url);
+        }catch (Exception e){
+            Log.d(FEED_URL, "Error en Asing task");
+            e.printStackTrace();
+        }
+
     }
 
 
